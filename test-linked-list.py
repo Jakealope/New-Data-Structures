6dfaf005480e54520b4851a1f97b915d50c152b9
Jakealope <jake.anderson486@gmail.com>
2015-04-16 14:35:07 -0700

    added two tests

--- test-linked-list.py
+++ test-linked-list.py
@@ -1,1 +1,15 @@
 import pytest
+
+# This will be the tests for a linked list
+
+
+def test_node_data():
+    new = Node("Bob")
+    assert new.data == "Bob"
+
+
+def test_node_add():
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    assert new.firstNode.data == "Bob"
