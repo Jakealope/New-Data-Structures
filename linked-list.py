bd3d45ac973a4373d3092be87d063342a1d6798a
Jakealope <jake.anderson486@gmail.com>
2015-04-16 14:41:37 -0700

    added LinkedList class, __init__ and insert classes

--- linked-list.py
+++ linked-list.py
@@ -5,3 +5,16 @@ class Node(object):
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
+
+
+class LinkedList(object):
+    def __init__(self, first=None):
+        self.first = first
+
+    def insert(self, new):
+        # insert new at beginning of list
+        if not self.first:
+            self.first = new
+        else:
+            new.next_node = self.first
+            self.first = new
