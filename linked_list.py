43b75479588a1ff63ad01b9d0383086f26761dd7
Jakealope <jake.anderson486@gmail.com>
2015-04-16 14:53:48 -0700

    added size function

--- linked_list.py
+++ linked_list.py
@@ -2,9 +2,9 @@
 
 
 class Node(object):
-    def __init__(self, val, next_node=None):
+    def __init__(self, val, _next=None):
         self.val = val
-        self.next_node = next_node
+        self._next = _next
 
 
 class LinkedList(object):
@@ -16,11 +16,20 @@ class LinkedList(object):
         if not self.first:
             self.first = new
         else:
-            new.next_node = self.first
+            new._next = self.first
             self.first = new
 
     def pop(self):
         # pops first item from list and returns it
-        chopped = self.firstNode
-        self.firstNode = self.firstNode.nextNode
+        chopped = self.first
+        self.first = self.first._next
         return chopped.val
+
+    def size(self):
+        # returns length of list
+        current = self.first
+        size = 0
+        while current is not None:
+            size += 1
+            current = current._next
+        return size
