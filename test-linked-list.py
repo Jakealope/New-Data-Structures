04338df665c5169531ac2ea87d2ec57b1185f3b4
Jakealope <jake.anderson486@gmail.com>
2015-04-16 14:36:26 -0700

    added tests

--- test-linked-list.py
+++ test-linked-list.py
@@ -13,3 +13,19 @@ def test_node_add():
     new = LinkedList()
     new.insert(bob)
     assert new.firstNode.data == "Bob"
+
+
+def test_next_node_none():
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    assert new.firstNode.nextNode is None
+
+
+def test_add_second_node():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    assert new.firstNode.nextNode.data == "Bob"
