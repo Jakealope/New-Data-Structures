8060c597a2136911d66f28cc18201c77d609627f
Jakealope <jake.anderson486@gmail.com>
2015-04-16 15:15:34 -0700

    added tests for pop, size, and search

--- test-linked-list.py
+++ test-linked-list.py
@@ -1,26 +1,26 @@
-from linked_list.py import Node, LinkedList
+from linked_list import Node, LinkedList
 import pytest
 
 # This will be the tests for a linked list
 
 
-def test_node_data():
+def test_node_val():
     new = Node("Bob")
-    assert new.data == "Bob"
+    assert new.val == "Bob"
 
 
 def test_node_add():
     bob = Node("Bob")
     new = LinkedList()
     new.insert(bob)
-    assert new.firstNode.data == "Bob"
+    assert new.first.val == "Bob"
 
 
 def test_next_node_none():
     bob = Node("Bob")
     new = LinkedList()
     new.insert(bob)
-    assert new.firstNode.nextNode is None
+    assert new.first._next is None
 
 
 def test_add_second_node():
@@ -29,4 +29,50 @@ def test_add_second_node():
     new = LinkedList()
     new.insert(bob)
     new.insert(joe)
-    assert new.firstNode.nextNode.data == "Bob"
+    assert new.first._next.val == "Bob"
+
+
+def test_pop():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    assert new.pop() == "Joe"
+
+
+def test_pop_size():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    new.pop()
+    assert new.size() == 1
+
+
+def test_size():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    assert new.size() == 2
+
+
+def test_search_fail():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    assert new.search("Fred") is None
+
+
+def test_search_success():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    new = LinkedList()
+    new.insert(bob)
+    new.insert(joe)
+    assert new.search("Bob") == bob
