02ae025488183b8e4ed4ea0d11fa7e7b0a7cff20
Jakealope <jake.anderson486@gmail.com>
2015-04-17 14:30:54 -0700

    added more tests

--- test_parenthetics.py
+++ test_parenthetics.py
@@ -13,3 +13,43 @@ def test_isneg():
 
 def test_iszero():
     assert is_balanced("( )") == 0
+
+
+def test_withstring():
+    assert is_balanced("this is a (string) with open and close") == 0
+
+
+def test_mismatched():
+    assert is_balanced("This is a string with )))))(((((( wrong order") == -1
+
+
+def test_bug():
+    assert is_balanced("()(") == 1
+
+
+def test_greater_than_one():
+    assert is_balanced("(((") == 1
+
+
+def test_three():
+    assert is_balanced("(((this is some text)))some more text") == 0
+
+
+def test_three_unballanced():
+    assert is_balanced("(((text))))(") == -1
+
+
+def test_empty():
+    assert is_balanced("") == 0
+
+
+def test_no_paren():
+    assert is_balanced("this is text no parenthesis") == 0
+
+
+def test_jumbled_up():
+    assert is_balanced("(()()))()") == -1
+
+
+def test_will_it_break():
+    assert is_balanced("()())(())(())(((") == -1
