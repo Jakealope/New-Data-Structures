b5e53b0290e4f2044c2b3c61eaf175833450287b
Jakealope <jake.anderson486@gmail.com>
2015-04-20 10:49:56 -0700

    added tests for appending single and multi

--- test_d_linked_list.py
+++ test_d_linked_list.py
@@ -7,7 +7,7 @@ import pytest
 def test_insert_one():
     linked = DLinked()
     linked.insert("Bob")
-    assert linked.first_item.val == "Bob"
+    assert linked.first.val == "Bob"
 
 
 def test_insert_multi():
@@ -15,4 +15,18 @@ def test_insert_multi():
     linked.insert("Fred")
     linked.insert("Bob")
     linked.insert("Joe")
-    assert linked.first_item.val == "Joe"
+    assert linked.first.val == "Joe"
+
+
+def test_append_one():
+    linked = DLinked()
+    linked.append("Bob")
+    assert linked.last.val == "Bob"
+
+
+def test_append_multi():
+    linked = DLinked()
+    linked.append("Fred")
+    linked.append("Bob")
+    linked.append("Joe")
+    assert linked.last.val == "Joe"
