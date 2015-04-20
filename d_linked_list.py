fbb6ad578f6b6392061b35f5ea1c706872db6f0d
Jakealope <jake.anderson486@gmail.com>
2015-04-20 10:48:41 -0700

    added parent class and worker class with insert funtion, passing

--- d_linked_list.py
+++ d_linked_list.py
@@ -1,1 +1,22 @@
 # This is a doubly linked list
+
+
+class ListItem(object):
+    def __init__(self, val, previous=None, next=None):
+        self.val = val
+        self.next = next
+        self.previous = previous
+
+
+class DLinked(object):
+    def __init__(self, first=None, last=None):
+        self.first = first
+        self.last = last
+
+    def insert(self, val):
+        # adds val to beginning of list
+        new_item = ListItem(val, next=self.first)
+        if self.first is None:
+            self.first = self.last = new_item
+        else:
+            self.first = self.first.previous = new_item
