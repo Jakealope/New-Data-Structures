6aec3fd1ae0c1467bdbd00aa07523caa58496a85
Jakealope <jake.anderson486@gmail.com>
2015-04-20 10:44:05 -0700

    added tests for single and multi insertion

--- test_d_linked_list.py
+++ test_d_linked_list.py
@@ -1,1 +1,18 @@
 # This file is for testing the Double Linked List
+
+from d_linked_list import DLinked
+import pytest
+
+
+def test_insert_one():
+    linked = DLinked()
+    linked.insert("Bob")
+    assert linked.first_item.val == "Bob"
+
+
+def test_insert_multi():
+    linked = DLinked()
+    linked.insert("Fred")
+    linked.insert("Bob")
+    linked.insert("Joe")
+    assert linked.first_item.val == "Joe"
