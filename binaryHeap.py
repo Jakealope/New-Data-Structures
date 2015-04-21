9c90cbae0acdacdc00590ecf358081d83479a9b0
Jakealope <jake.anderson486@gmail.com>
2015-04-21 15:47:13 -0700

    added push function

--- binaryHeap.py
+++ binaryHeap.py
@@ -5,3 +5,8 @@ class BinaryHeap(object):
     def __init__(self):
         self.heaplist = [0]
         self.currentsize = 0
+
+    def push(self, val):
+        self.heaplist.append(val)
+        self.currentsize += 1
+        self.bubble_up(self.currentsize)
