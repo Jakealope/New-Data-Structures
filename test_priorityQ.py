4e2d03ede25b9940aa836bb441d81c9c0b72ad59
Jakealope <jake.anderson486@gmail.com>
2015-04-21 14:06:28 -0700

    added tests for pop and peak

--- test_priorityQ.py
+++ test_priorityQ.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 
-class QueueTest():
+class StackTest():
 
     def test_init(self):
         queue = pq.Priority()
@@ -11,3 +11,11 @@ class QueueTest():
 
     def test_insert(self):
         self.assertEqual(self.append.item)
+
+    def test_pop(self):
+        self.asserEqual(self.pop.item)
+        self.assertEqual(self.length == 0)
+
+    def test_peek(self):
+        self.asserEqual(self.append.item)
+        self.assertEqual(self.peek == 0)
