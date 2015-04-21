0de51e58a9aefd71411cd61d747487f139388bde
Jakealope <jake.anderson486@gmail.com>
2015-04-21 13:34:43 -0700

    added Queue class and enqueue function, passing

--- queue.py
+++ queue.py
@@ -5,3 +5,18 @@ class QueueItem(object):
         self.val = val
         self.next = next
         self.prev = prev
+
+
+class Queue(object):
+    def __init__(self, first=None, last=None):
+        self.first = first
+        self.last = last
+
+    def enqueue(self, val):
+        # adds val to beginning of queue
+        new = QueueItem(val, next=self.last)
+        if not self.first:
+            self.first = self.last = new
+        else:
+            self.last.prev = new
+            self.last = new
