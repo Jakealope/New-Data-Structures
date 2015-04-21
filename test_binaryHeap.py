c9a753d20bbd5b108ad4f2443e221084756b898f
Jakealope <jake.anderson486@gmail.com>
2015-04-21 15:50:32 -0700

    added test to make sure the heap contains all the right values and check the ordering of the heap

--- test_binaryHeap.py
+++ test_binaryHeap.py
@@ -11,3 +11,20 @@ def test_one_push_pop():
     bin_list = BinaryHeap()
     bin_list.push(10)
     assert bin_list.pop() == 10
+
+
+def all_items(heap):
+    items = []
+    while True:
+        try:
+            items.append(heap.pop())
+        except IndexError:
+            return items
+    return items
+
+
+def test_reverse_range():
+    heap = BinaryHeap()
+    for i in range(5, 0, -1):
+        heap.push(i)
+    assert all_items(heap) == range(1, 6)
