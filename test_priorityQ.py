d76f1a347a6d8582a22ff74051bedd889e48b555
Jakealope <jake.anderson486@gmail.com>
2015-04-21 14:02:15 -0700

    added test for insert

--- test_priorityQ.py
+++ test_priorityQ.py
@@ -8,3 +8,6 @@ class QueueTest():
     def test_init(self):
         queue = pq.Priority()
         assert type(queue) == pq.Priority
+
+    def test_insert(self):
+        self.assertEqual(self.append.item)
