816c929f20bfa55f3bce630126df6fb5eed1cdb9
Jakealope <jake.anderson486@gmail.com>
2015-04-22 14:31:59 -0700

    added class and init func/add func

--- graph.py
+++ graph.py
@@ -1,3 +1,13 @@
-# a simple graph
+# a simple graph that will allow us to impliment a graph data structure
 
 import heapq
+
+
+class SimpleGraph(object):
+
+    def __init__(self, edges=None):
+        self.dict_graph = {}
+
+    def nodes(self):
+        '''return a iterator through of all nodes in the graph'''
+        return self.dict_graph.iterkeys()
