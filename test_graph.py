1020a288a400655d69f24375cbbe44b570abf9c8
Jakealope <jake.anderson486@gmail.com>
2015-04-22 14:28:41 -0700

    added test to delete node

--- test_graph.py
+++ test_graph.py
@@ -15,3 +15,16 @@ def test_add_nodes():
     assert 'a' in g.nodes()
     assert 'b' in g.nodes()
     assert 'c' in g.nodes()
+
+
+def test_del_nodes():
+    """nodes are deleted"""
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    g.del_node('a')
+    assert 'a' not in g.nodes()
+    assert g.has_node('a') is False
