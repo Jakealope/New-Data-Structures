5966c7dac6a8aa160d1bcff2e7c5e7627612a3d0
Jakealope <jake.anderson486@gmail.com>
2015-04-22 14:26:40 -0700

    added test for adding node

--- test_graph.py
+++ test_graph.py
@@ -2,3 +2,16 @@
 
 from graph import SimpleGraph
 import pytest
+
+
+def test_add_nodes():
+    """nodes are added"""
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    assert 'a' in g.nodes()
+    assert 'b' in g.nodes()
+    assert 'c' in g.nodes()
