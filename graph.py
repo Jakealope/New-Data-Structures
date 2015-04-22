a3527271c9305975bebcb6acb4910b113e91124f
Jakealope <jake.anderson486@gmail.com>
2015-04-22 14:33:22 -0700

    added edge function that returns a list of edges in graph

--- graph.py
+++ graph.py
@@ -11,3 +11,11 @@ class SimpleGraph(object):
     def nodes(self):
         '''return a iterator through of all nodes in the graph'''
         return self.dict_graph.iterkeys()
+
+    def edges(self):
+        '''return a list of all edges in the graph'''
+        edges = []
+        for key, value in self.dict_graph.iteritems():
+            for item in value:
+                edges.append((key, item))
+        return edges
