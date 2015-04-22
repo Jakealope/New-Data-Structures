bc03767fb2e3be318c4cade495907b227ccb1e19
Jakealope <jake.anderson486@gmail.com>
2015-04-22 14:44:02 -0700

    added test to check delete error

--- test_graph.py
+++ test_graph.py
@@ -28,3 +28,15 @@ def test_del_nodes():
     g.del_node('a')
     assert 'a' not in g.nodes()
     assert g.has_node('a') is False
+
+
+def test_del_error():
+    """error raised when non-existant value deleted"""
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    with pytest.raises(KeyError):
+        g.del_node('p')
